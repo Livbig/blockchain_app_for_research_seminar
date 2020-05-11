3123171c0d41680fbbdcf547c64f31953f7fde64
livbig <livbigovich@gmail.com>
2020-05-11 17:39:26 +0300

    THIRD

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:convert/convert.dart';
 import 'package:crypto/crypto.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
+import 'package:qrcode/qrcode.dart';
+import 'package:qr/qr.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,78 +32,82 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   final myController1= TextEditingController(text: "admin");
   final myController2= TextEditingController(text: "password");
   hash(foo) => sha256.convert(utf8.encode(foo));
+  
   authorize(){
     final checkuser = hash("admin");
     final checkpass = hash("password");
     var username = hash(myController1.text);
     var password = hash(myController2.text);
-    if(checkuser == username && checkpass == password){
+    if(checkuser == username && checkpass == password){choice();}}
+  qrGen(){
+    print('yes');
+    showDialog(context: context, builder: (BuildContext context){
       return SimpleDialog(
-        title: const Text('Select one.'),
+        title: Text('Your QR code.', style: TextStyle(color: Colors.black),),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        backgroundColor: Colors.white,
         children: <Widget>[
-          SimpleDialogOption(
-            onPressed: () { Navigator.pop(context); },
-            child: FloatingActionButton(
-              child: Icon(Icons.arrow_downward),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: PrettyQr(
+                //image: AssetImage('assets/blockchain.png'),
+                typeNumber: 5,
+                size: 250,
+                data: '${hash("password")}',
+                errorCorrectLevel: QrErrorCorrectLevel.M,
+                roundEdges: true),
+          ),
+          ],
+      );
+    });
+  }
+  choice(){
+    showDialog(context: context, builder: (BuildContext context){
+      return SimpleDialog(
+          title: const Text('Select one.'),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {},
+              child: FloatingActionButton.extended(
+                  label: Text("Give"),
+                  icon: Icon(Icons.arrow_upward), onPressed: () {},),
             ),
-          ),
-          SimpleDialogOption(
-            onPressed: () { Navigator.pop(context); },
-            child: FloatingActionButton(
-              child: Icon(Icons.arrow_upward),),
-          ),
-    ]);
-    } else{
-      print('no');
-      print(hash("admin"));
-      print(hash(myController1.text));
+            SimpleDialogOption(
+              onPressed: () {},
+              child: FloatingActionButton.extended(
+                  label: Text("Get"),
+                  icon: Icon(Icons.arrow_downward), onPressed: () {qrGen();},),
+            ),]
+      );
     }
+    );
+  }
 
   void dispose() {
     // Clean up the controller when the widget is disposed.
     myController1.dispose();
     myController2.dispose();
     super.dispose();
-  }}
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
